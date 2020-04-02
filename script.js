bc1c63c86322fd46b341707ac0c679e8daa3fa9c
Volchonok <36218165+isawunicorn@users.noreply.github.com>
2020-04-02 12:35:54 +0300

    add ship

---
+++ script.js
@@ -0,0 +1,29 @@
+var ship;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight - 3);
+    ship = new Ship();
+}
+
+function draw() {
+    background(0);
+    ship.render();
+    ship.turn();
+    ship.update();
+    ship.edges();
+}
+
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
+
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    } else if (keyCode == UP_ARROW) {
+        ship.boosting(true);
+    }
+}
